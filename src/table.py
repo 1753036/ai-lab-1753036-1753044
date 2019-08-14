dbb623898b6c33b54289a4ba2851d12b5f858777
zissalg <thai.cuong1404@gmail.com>
2019-08-14 18:20:27 +0700

    add table

---
+++ src/table.py
@@ -0,0 +1,8 @@
+ 
+class Table:
+    def __init__(self, row_count, col_count):
+        self.row_count = row_count
+        self.col_count = col_count
+        # self.data_table = 
+
+    def print():
